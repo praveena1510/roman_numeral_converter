2e86435de89becda9d0df492e201fd2bb8a6b0bd
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-08-24 16:17:06 +0100

    Refactored convert_integer_to_roman_numerals

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,9 +1,9 @@
 def convert_integer_to_roman_numerals(input_integer):
-    if input_integer == 6:
-        return "VI"
-    if input_integer == 5:
-        return "V"
+    str = ""
+    if input_integer>=5:
+       str =  str + "V"
+       input_integer= input_integer - 5
     if input_integer == 4:
         return "IV"
-    return "I" * input_integer
+    return str + "I" * input_integer
 
