70fe38cd8b22289a3adc87428d2de34584ab8af1
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-08-24 15:54:59 +0100

    reformatted test cases to separate lines to distinguish

--- test/test_roman_conversion_to_numerals.py
+++ test/test_roman_conversion_to_numerals.py
@@ -7,6 +7,11 @@ from src.integer_to_roman_numerals_converter import convert_integer_to_roman_num
 
 class IntegerToRomanNumeralsTest(unittest.TestCase):
 
-    @parameterized.expand([(1, "I"), (2, "II"), (3, "III"), (4, "IV")])
+    @parameterized.expand([
+        (1, "I"),
+        (2, "II"),
+        (3, "III"),
+        (4, "IV")
+       ])
     def test_converting_integer_to_roman_numerals(self, input_integer: int, expected_roman_numeral: str):
         self.assertEqual(convert_integer_to_roman_numerals(input_integer), expected_roman_numeral)
