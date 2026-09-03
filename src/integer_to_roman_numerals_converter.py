992614c387c7a070539d240b2960b8a78fa37693
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-09-03 16:18:26 +0100

    Added for loop for concatenating roman numerals in a table

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,12 +1,8 @@
 def convert_integer_to_roman_numerals(input_integer):
     roman_numerals = ""
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer,roman_numerals,50,"L")
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 40, "XL")
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 10, "X")
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 9, "IX")
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 5, "V")
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 4, "IV")
-    input_integer,roman_numerals = concatenate_roman_numeral (input_integer,roman_numerals,1, "I")
+    numeral_value_mapping = [(50, "L"), (40, "XL"),(10,"X"),(9, "IX"), (5, "V"),(4, "IV"), (1,"I")]
+    for value, numeral in numeral_value_mapping:
+        input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, value, numeral)
     return roman_numerals
 
 
@@ -15,4 +11,3 @@ def concatenate_roman_numeral(input_integer, roman_numerals, value_of_numeral, n
         roman_numerals = roman_numerals + numeral
         input_integer = input_integer - value_of_numeral
     return input_integer, roman_numerals
-
