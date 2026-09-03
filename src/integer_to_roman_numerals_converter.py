b2543e066028f62da150cfa944d439e336c58390
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-03 16:06:38 +0100

    Added test for numeral 40

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,11 +1,13 @@
 def convert_integer_to_roman_numerals(input_integer):
     roman_numerals = ""
+    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 40, "XL")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 10, "X")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 9, "IX")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 5, "V")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 4, "IV")
-    input_integer,roman_numerals = concatenate_roman_numeral (input_integer,roman_numerals,1,"I")
+    input_integer,roman_numerals = concatenate_roman_numeral (input_integer,roman_numerals,1, "I")
     return roman_numerals
+
 
 def concatenate_roman_numeral(input_integer, roman_numerals, value_of_numeral, numeral):
     while input_integer >= value_of_numeral:
--- test/test_roman_conversion_to_numerals.py
+++ test/test_roman_conversion_to_numerals.py
@@ -20,6 +20,7 @@ class IntegerToRomanNumeralsTest(unittest.TestCase):
         (14, "XIV"),
         (19, "XIX"),
         (20, "XX"),
+        (40, "XL")
        ])
     def test_converting_integer_to_roman_numerals(self, input_integer: int, expected_roman_numeral: str):
         self.assertEqual(convert_integer_to_roman_numerals(input_integer), expected_roman_numeral)
