f8e5062be9491cd3c6e7288cb8936f0b67b7e9c5
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-03 15:33:14 +0100

    Refactored convert_integer_to_roman_numerals logic in a new method

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,8 +1,6 @@
 def convert_integer_to_roman_numerals(input_integer):
     roman_numeral = ""
-    if input_integer >= 10:
-        roman_numeral = roman_numeral + "X"
-        input_integer = input_integer - 10
+    input_integer, roman_numeral = concatenate_roman_numeral(input_integer, roman_numeral)
     if input_integer >= 9:
         roman_numeral = roman_numeral + "IX"
         input_integer = input_integer - 9
@@ -13,3 +11,11 @@ def convert_integer_to_roman_numerals(input_integer):
         roman_numeral = roman_numeral + "IV"
         input_integer = input_integer - 4
     return roman_numeral + "I" * input_integer
+
+
+def concatenate_roman_numeral(input_integer, roman_numeral):
+    if input_integer >= 10:
+        roman_numeral = roman_numeral + "X"
+        input_integer = input_integer - 10
+    return input_integer, roman_numeral
+
