faf9c0551713c9ccf51491891d69b3fd619ef875
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-03 15:43:45 +0100

    Removed the default value_of_numeral

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,6 +1,6 @@
 def convert_integer_to_roman_numerals(input_integer):
     roman_numerals = ""
-    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals)
+    input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 10)
     if input_integer >= 9:
         roman_numerals = roman_numerals + "IX"
         input_integer = input_integer - 9
@@ -13,7 +13,7 @@ def convert_integer_to_roman_numerals(input_integer):
     return roman_numerals + "I" * input_integer
 
 
-def concatenate_roman_numeral(input_integer, roman_numerals, value_of_numeral=10):
+def concatenate_roman_numeral(input_integer, roman_numerals, value_of_numeral):
     if input_integer >= value_of_numeral:
         roman_numerals = roman_numerals + "X"
         input_integer = input_integer - value_of_numeral
