7c0e4bb6f41806721f27f5ff325e6070337158ae
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-09-03 16:02:40 +0100

    reused concatenate_roman_numeral for X and I

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -4,8 +4,8 @@ def convert_integer_to_roman_numerals(input_integer):
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 9, "IX")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 5, "V")
     input_integer, roman_numerals = concatenate_roman_numeral(input_integer, roman_numerals, 4, "IV")
-    return roman_numerals + "I" * input_integer
-
+    input_integer,roman_numerals = concatenate_roman_numeral (input_integer,roman_numerals,1,"I")
+    return roman_numerals
 
 def concatenate_roman_numeral(input_integer, roman_numerals, value_of_numeral, numeral):
     while input_integer >= value_of_numeral:
