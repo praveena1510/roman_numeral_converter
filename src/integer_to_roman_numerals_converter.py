92da0b11180136955687c276548153bf728657fa
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-03 14:24:20 +0100

    Refracted the code

--- src/integer_to_roman_numerals_converter.py
+++ src/integer_to_roman_numerals_converter.py
@@ -1,11 +1,10 @@
 def convert_integer_to_roman_numerals(input_integer):
-    str = ""
+    roman_numeral = ""
     if input_integer == 9:
         return "IX"
-    if input_integer>=5:
-       str =  str + "V"
-       input_integer= input_integer - 5
+    if input_integer >= 5:
+        roman_numeral = roman_numeral + "V"
+        input_integer = input_integer - 5
     if input_integer == 4:
         return "IV"
-    return str + "I" * input_integer
-
+    return roman_numeral + "I" * input_integer
